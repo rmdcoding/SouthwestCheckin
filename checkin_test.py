3efca28cd7b13189dff2e34ba05ee48a5b51368e
rmdcoding <rmdcoding@gmail.com>
2019-08-12 12:59:43 -0700

    Updating test file with new URL for API change.

--- checkin_test.py
+++ checkin_test.py
@@ -6,7 +6,7 @@ from pytz import timezone
 
 @requests_mock.Mocker()
 def test_checkin(m):
-    m.register_uri('GET', '/api/mobile-misc/v1/mobile-misc/page/view-reservation/XXXX?first-name=John&last-name=Smith', text=open('fixtures/view-reservation.json', 'r').read())
+    m.register_uri('GET', '/api/mobile-air-booking/v1/mobile-air-booking/page/view-reservation/XXXX?first-name=John&last-name=Smith', text=open('fixtures/view-reservation.json', 'r').read())
     m.register_uri('GET', '/api/mobile-air-operations/v1/mobile-air-operations/page/check-in/XXXX?first-name=John&last-name=Smith', text=open('fixtures/checkin-get.json', 'r').read())
     m.register_uri('POST', '/api/mobile-air-operations/v1/mobile-air-operations/page/check-in', text=open('fixtures/checkin-post.json', 'r').read())
     assert checkin.auto_checkin('XXXX', 'John', 'Smith') == None
